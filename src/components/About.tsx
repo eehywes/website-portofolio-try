1d11ac56dad20770c0c97bbbd816168c8ceb0687
eehywes <120404886+eehywes@users.noreply.github.com>
2025-10-15 04:24:12 +0700

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,19 +5,19 @@ const About = () => {
         <div className="text-center mb-12">
           <h2 className="text-4xl md:text-5xl font-bold mb-4">About Me</h2>
           <p className="text-muted-foreground">
-            Saya adalah Mahasiswa Teknik Informatika Universitas Pelita Bangsa Semarang
+            Saya adalah Mahasiswa Teknik Informatika Universitas Wahid Hasyim Semarang
           </p>
         </div>
         
         <div className="glass-card rounded-3xl p-8 md:p-12 space-y-6">
           <p className="text-foreground/80 leading-relaxed">
-            Halo! Nama Saya Amirul Fahad Lahir di Kendal, 05 Januari 2025. Aamiin saya di Desa Tasikagung Wetan Kangkung Kendal. Saya 
-            Mahasiswa Teknik Informatika Universitas Pelita Bangsa Semarang
+            Hallo! Nama Saya Alaika Amirul Fahad Lahir di Kendal, 05 Januari 2025, Alamat saya di Desa Gebanganom Wetan Kangkung Kendal, 
+            Saya Mahasiswa Teknik Informatika Universitas Wahid Hasyim Semarang
           </p>
           
           <p className="text-foreground/80 leading-relaxed">
-            Saya Memiliki Ketertarikan terhadap komputer karena sejak saya berkuliah, sehingga enchantman saya juga menantang terhadapan Problem Solving yang bagus, 
-            menarik dan interaktif. Sampai saya menemukan sebuah Alat berupa Whatsapp.. Saya juga mereka berada dan terakir jatuh hati.
+            Saya memiliki Kemampuan berpikir kreatif, kritis dan terbuka, sebagai mahasiswa saya juga mempunya kemampuan Problem Solving yang bagus, memiliki minat di bidang Fotografi dan Wirausaha. 
+            Saya Juga memiliki bisnis Jual Beli Iphone 
           </p>
         </div>
       </div>
